a8ee820e443eec434d097455471b2a14487c4ecb
league <league@wts14-3.attlocal.net>
2025-09-13 16:29:10 -0700

    Started arrays

--- src/_00_Intro_To_Arrays/_01_IntroToArrays.java
+++ src/_00_Intro_To_Arrays/_01_IntroToArrays.java
@@ -5,19 +5,28 @@ import java.util.Random;
 public class _01_IntroToArrays {
     public static void main(String[] args) {
         // 1. declare and Initialize an array 5 Strings
-
+    	String[] teams = new String[5];
+    	teams[0]="Knicks";
+    	teams[1]="Chiefs";
+    	teams[2]="Maple Leafs";
+    	teams[3]="Cubs";
+    	teams[4]="Inter Miami";
         // 2. print the third element in the array
-
+    	System.out.println(teams[3]);
         // 3. set the third element to a different value
-
+    	teams[3]="Padres";
         // 4. print the third element again
-
+    	System.out.println(teams[3]);
         // 5. use a for loop to set all the elements in the array to a string
         //    of your choice
-
+    	for(int i=0; i<5; i++) {
+    		teams[i]="sports";
+    	}
         // 6. use a for loop to print all the values in the array
         //    BE SURE TO USE THE ARRAY'S length VARIABLE
-
+    	for(int i=0; i<5; i++) {
+    		System.out.println(teams[i]);
+    	}
         // 7. make an array of 50 integers
 
         // 8. use a for loop to make every value of the integer array a random
